05cac705f077f43edcc03df86e55f388949c2f1c
TJ Reynolds <reynolds6404@gmail.com>
2020-04-30 16:43:20 -0700

    Update sp2_picker.py
    bug fix

--- sp2_picker.py
+++ sp2_picker.py
@@ -2,20 +2,20 @@ import random
 
 countryFile = open('sp2_countries.txt', 'r')
 countryList = countryFile.readlines()
-print(len(countryList)-2)
 numPlayers = input('insert number of players: ')
 
 allchosen = []
 for player in range(int(numPlayers)):
     chosen = []
     for i in range(0, 10):
-        choice = countryList[random.randint(0, len(countryList)-2)]
+        choice = countryList[random.randint(0, len(countryList)-1)]
         truth = True
         while truth:
             if(choice[0:len(choice)-1] in chosen) or (choice[0:len(choice)-1] in allchosen):
-                choice = countryList[random.randint(0, len(countryList)-2)]
+                choice = countryList[random.randint(0, len(countryList)-1)]
             else:
                 truth = False
                 chosen.append(choice[0:len(choice)-1])
                 allchosen.append(choice[0:len(choice)-1])
+
     print(chosen)
