942e9a0fbe7f65b1fef878ef056c756271987687
TJ Reynolds <reynolds6404@gmail.com>
2020-04-30 19:55:03 -0700

    Update sp2_picker.py
    optimized for a discord bot

--- sp2_picker.py
+++ sp2_picker.py
@@ -2,20 +2,21 @@ import random
 
 countryFile = open('sp2_countries.txt', 'r')
 countryList = countryFile.readlines()
-numPlayers = input('insert number of players: ')
 
-allchosen = []
-for player in range(int(numPlayers)):
-    chosen = []
-    for i in range(0, 10):
-        choice = countryList[random.randint(0, len(countryList)-1)]
-        truth = True
-        while truth:
-            if(choice[0:len(choice)-1] in chosen) or (choice[0:len(choice)-1] in allchosen):
-                choice = countryList[random.randint(0, len(countryList)-1)]
-            else:
-                truth = False
-                chosen.append(choice[0:len(choice)-1])
-                allchosen.append(choice[0:len(choice)-1])
-
-    print(chosen)
+def pick(*args):
+    allchosen = []
+    listArr = []
+    for arg in args:
+        chosen = []
+        for i in range(0, 10):
+            choice = countryList[random.randint(0, len(countryList)-1)]
+            truth = True
+            while truth:
+                if(choice[0:len(choice)-1] in chosen) or (choice[0:len(choice)-1] in allchosen):
+                    choice = countryList[random.randint(0, len(countryList)-1)]
+                else:
+                    truth = False
+                    chosen.append(choice[0:len(choice)-1])
+                    allchosen.append(choice[0:len(choice)-1])
+        listArr.append(chosen)
+    return listArr
